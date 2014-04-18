4015780bb42b0afa711cf0b2c529f410bbd6afc7
Moirae <is.moirae@gmail.com>
2014-04-18 11:19:14 +0200

    Version +1

--- version.php
+++ version.php
@@ -22,5 +22,5 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = '2014032600';
+$plugin->version = '2014032601';
 $plugin->requires = '2013111800'; //Moodle 2.6. release version
