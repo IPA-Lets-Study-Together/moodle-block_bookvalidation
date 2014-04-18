bb005233878b7dfcfc7d84522cc753ac35081079
Moirae <is.moirae@gmail.com>
2014-04-18 11:19:22 +0200

    Editing view

--- view.php
+++ view.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('../../config.php');
-//require_once('bookvalidation_form.php');
+require_once('bookvalidation_form.php');
 
 global $DB, $USER, $OUTPUT, $PAGE;
 
@@ -16,29 +16,9 @@ require_login($course);
 
 $PAGE->set_url('/blocks/bookvalidation/view.php', array('id'=>$courseid));
 $PAGE->set_pagelayout('standard');
-$PAGE->set_heading(get_string('bookvalidation', 'block_simplehtml'));
+$PAGE->set_heading(get_string('bookvalidation', 'block_bookvalidation'));
 
-//$bookvalidation = new bookvalidation_form();
-
-$query = 'SELECT cu.fullname
-				FROM {course} cu
-				JOIN (
-
-					SELECT u.id AS userid, u.firstname AS name, mrs.contextid, mrs.roleid, c.instanceid
-					FROM {user} u
-					JOIN {role_assignments} mrs ON mrs.userid = u.id
-					JOIN {context} c ON c.id = mrs.contextid
-					WHERE u.id = ?
-					AND (
-					mrs.roleid =3
-					OR mrs.roleid =4
-					OR mrs.roleid =1
-					) AND c.contextlevel =50
-				) AS part ON part.instanceid = cu.id';
-
-		$data = $DB->get_records_sql($query, array($userid));
-
-		var_dump($data);
+$bookvalidation = new bookvalidation_form();
 
 echo $OUTPUT->header();
 $bookvalidation->display();
